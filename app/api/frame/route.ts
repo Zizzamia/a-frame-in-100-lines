738468016992c59fa1df3656caac5c385e68e517
mmurrs <mmurrayc@umich.edu>
2024-02-02 19:17:55 -0800

    added second button

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,12 +5,16 @@ const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
+  let button_2: bool | undefined = '';
+  let button_3: string | undefined = '';
+  let button_4: string | undefined = '';
 
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   if (isValid) {
     accountAddress = message.interactor.verified_accounts[0];
+    button_2 = message.following;
   }
 
   return new NextResponse(
@@ -19,6 +23,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         {
           label: `🌲 ${accountAddress} 🌲`,
         },
+        {
+          label: `${button_2}`,
+        }
       ],
       image: `https://spotify-gallery-00.vercel.app/ying_yang_mid.png`,
       post_url: `https://spotify-gallery-00.vercel.app/api/frame`,
