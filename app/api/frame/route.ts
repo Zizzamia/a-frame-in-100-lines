288c3d0fcd446bbeda6ac969a8b9f4d1b1104104
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-02 17:45:59 -0800

    feat: latest kit

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -7,7 +7,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
   let text: string | undefined = '';
 
-  const body: FrameRequest = await req.json();
+  const body: any = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   if (isValid) {
