db2cf9c26a185864c5f5b16a72c49ef9c0b9c08f
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-02 18:02:55 -0800

    dope

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -7,13 +7,14 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
   let text: string | undefined = '';
 
-  const body: any = await req.json();
+  const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   if (isValid) {
     accountAddress = message.interactor.verified_accounts[0];
   }
-  if (body.untrustedData.inputText) {
+
+  if (body?.untrustedData?.inputText) {
     text = body.untrustedData.inputText;
   }
 
@@ -21,7 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `${text}`,
+          label: `Text: ${text}`,
         },
       ],
       image: `${NEXT_PUBLIC_URL}/park-2.png`,
