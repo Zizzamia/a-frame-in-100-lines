5412255b80863ce10422a7b121501291f4dee963
ssolson <sterlingolson@yahoo.com>
2024-02-11 16:19:22 -0700

    fix edge export

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse } from '@vercel/og';
 
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
 
 import { NextRequest, NextResponse } from 'next/server';
 
