66a8ed7b74746165212c2a4db2fd4872aa9312d7
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-18 14:50:42 -0800

    chore: latest kit v0.7

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -28,7 +28,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `Story: ${text} 🌲`,
+          label: `Story: ${text} 🌲🌲`,
         },
       ],
       image: {
