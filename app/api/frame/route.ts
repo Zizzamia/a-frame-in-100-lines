8881e7edf254f32314efd9f51d6d36a2f4121ffb
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-02-22 01:21:45 -0800

    chore: latest kit

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,4 +1,4 @@
-import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/core';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 
