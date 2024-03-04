60fcd3d8b095c9fc252311e5c934cf3e5e34ed7c
Samuel Huber <40248495+SamuelLHuber@users.noreply.github.com>
2024-03-04 20:42:07 +0100

    feat: use NODE_ENV to auto switch localhost or prod (#10)

--- app/config.ts
+++ app/config.ts
@@ -1,1 +1,2 @@
-export const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
+// use NODE_ENV to not have to change config based on where it's deployed
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV == "development" ? 'http://localhost:3000' : 'https://zizzamia.xyz';
