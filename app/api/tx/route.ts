5164ecda6eca51586a2f7b0bfa5cd11fdb353570
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-05-06 11:48:20 -0700

    chore: update pkg

--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -1,13 +1,14 @@
 import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 import { encodeFunctionData, parseEther } from 'viem';
-import { base } from 'viem/chains';
+import { baseSepolia } from 'viem/chains';
 import BuyMeACoffeeABI from '../../_contracts/BuyMeACoffeeABI';
 import { BUY_MY_COFFEE_CONTRACT_ADDR } from '../../config';
 import type { FrameTransactionResponse } from '@coinbase/onchainkit/frame';
 
 async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
   const body: FrameRequest = await req.json();
+  // Remember to replace 'NEYNAR_ONCHAIN_KIT' with your own Neynar API key
   const { isValid } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   if (!isValid) {
@@ -21,7 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
   });
 
   const txData: FrameTransactionResponse = {
-    chainId: `eip155:${base.id}`, // Remember Base Sepolia might not work on Warpcast yet
+    chainId: `eip155:${baseSepolia.id}`,
     method: 'eth_sendTransaction',
     params: {
       abi: [],
