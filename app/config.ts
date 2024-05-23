e736b95c2c29abe1ee65125357962ffe1cfed9ee
Lawrence Kuang <lawrence.kuang@coinbase.com>
2024-05-23 14:06:11 -0700

    fix url

--- app/config.ts
+++ app/config.ts
@@ -2,5 +2,5 @@
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development'
     ? 'http://localhost:3000'
-    : 'a-frame-in-100-lines-ten-theta.vercel.app';
+    : 'https://a-frame-in-100-lines-ten-theta.vercel.app';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
