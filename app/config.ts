5dcbfadd75a87c90d421ff1ff7b236107f579336
aheesh <aheesh@gmail.com>
2024-06-30 14:51:19 -0700

    chore: Update NEXT_PUBLIC_URL in config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://blpframe.vercel.app/';
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://blpframe.vercel.app';
 export const PLAYER_A_CONTRACT_ADDR = '';
 export const STABLE_CONTRACT_ADDR = 0x1c7d4b196cb0c7b01d743fbc6116a902379c7238;
 export const BAL_VAULT_ADDR = '0xba12222222228d8ba445958a75a0704d566bf2c8';
