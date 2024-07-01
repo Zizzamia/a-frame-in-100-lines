8c684791673baab39825c4768ac9400956431fdd
aheesh <aheesh@gmail.com>
2024-06-30 17:11:26 -0700

    chore: Update tx route to use BigInt for value parameter in approve function

--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -16,22 +16,22 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
     return new NextResponse('Message not valid', { status: 500 });
   }
 
-  //const value = parseUnits('100', 6);
-  const value = BigInt('0x0000000000000000000000000000000000000000000000000000000005F5E100');
-  //let hexValue = toHex(value, { size: 32 }).slice(2);
+  const value = parseUnits('100', 6);
+  //const value = BigInt('0x0000000000000000000000000000000000000000000000000000000005F5E100');
+  let hexValue = BigInt('0x' + toHex(value, { size: 32 }).slice(2));
 
   const data = encodeFunctionData({
     abi: abi,
     functionName: 'approve',
-    args: [BAL_VAULT_ADDR, value],
+    args: [BAL_VAULT_ADDR, hexValue],
   });
 
-  const txData = {
+  const txData: FrameTransactionResponse = {
     chainId: `eip155:${sepolia.id}`,
     method: 'eth_sendTransaction',
     params: {
       abi: [],
-      data,
+      data: data,
       to: '0x1c7d4b196cb0c7b01d743fbc6116a902379c7238',
       value: '0',
     },
